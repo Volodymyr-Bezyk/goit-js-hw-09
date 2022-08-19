0e02ce995d6ee7677b1f038ffb0da78e54ed2fc3
Volodymyr-Bezyk <v.v.bezyk@gmail.com>
2022-08-19 15:51:00 +0300

    feature01/color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,39 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
+let changeColorIntervalId = null;
 
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function setBodyBgColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function btnSwitcher(e) {
+  switch (e.currentTarget) {
+    case refs.startBtn:
+      refs.startBtn.setAttribute('disabled', true);
+      refs.stopBtn.removeAttribute('disabled', true);
+      break;
+    case refs.stopBtn:
+      refs.startBtn.removeAttribute('disabled', true);
+      refs.stopBtn.setAttribute('disabled', true);
+      break;
+  }
+}
+
+function onStartBtnClick(e) {
+  changeColorIntervalId = setInterval(setBodyBgColor, 1000);
+  btnSwitcher(e);
+}
+
+function onStopBtnClick(e) {
+  clearInterval(changeColorIntervalId);
+  btnSwitcher(e);
+}
